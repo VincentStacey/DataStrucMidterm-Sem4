e947719ca1d48e8bedb86d44d702435dd0d25e1b
VincentStacey <Vincent.Stacey@keyin.com>
2025-02-27 20:03:29 -0330

    Task and User classes created

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,20 @@
 public class Task {
+    String description;
+    boolean isCompleted;
+    Task next;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+        this.next = null;
+    }
+
+    public void markAsCompleted() {
+        this.isCompleted = true;
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + (isCompleted ? "✔" : "✘") + " ] " + description;
+    }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,22 @@
 public class User {
+    String name;
+    TaskList taskList;
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public void addTask(String description) {
+        taskList.addTask(description);
+    }
+
+    public void markTaskCompleted(int index) {
+        taskList.markTaskCompleted(index);
+    }
+
+    public void printTasks() {
+        System.out.println("\nTasks for " + name + ":");
+        taskList.printTasks();
+    }
 }
