338e0a4b96f5379b67b7c5eb8047b8b150756393
VincentStacey <Vincent.Stacey@keyin.com>
2025-02-27 20:05:00 -0330

    TaskList and UserManager class files created.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,47 @@
 public class TaskList {
+    private Task head;
+
+    public void addTask(String description) {
+        Task newTask = new Task(description);
+        if (head == null) {
+            head = newTask;
+        } else {
+            Task temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newTask;
+        }
+        System.out.println("Task added successfully!");
+    }
+
+    public void markTaskCompleted(int index) {
+        Task temp = head;
+        int count = 1;
+
+        while (temp != null) {
+            if (count == index) {
+                temp.markAsCompleted();
+                System.out.println("Task marked as completed.");
+                return;
+            }
+            temp = temp.next;
+            count++;
+        }
+        System.out.println("Invalid task number.");
+    }
+
+    public void printTasks() {
+        if (head == null) {
+            System.out.println("No tasks found.");
+            return;
+        }
+        Task temp = head;
+        int index = 1;
+        while (temp != null) {
+            System.out.println(index + ". " + temp);
+            temp = temp.next;
+            index++;
+        }
+    }
 }
--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -1,2 +1,44 @@
 public class UserManager {
+    private User[] users;
+    private int userCount;
+
+    public UserManager(int maxUsers) {
+        users = new User[maxUsers];
+        userCount = 0;
+    }
+
+    public void addUser(String name) {
+        if (userCount >= users.length) {
+            System.out.println("User limit reached.");
+            return;
+        }
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].name.equals(name)) {
+                System.out.println("User already exists.");
+                return;
+            }
+        }
+        users[userCount++] = new User(name);
+        System.out.println("User added successfully.");
+    }
+
+    public User getUser(String name) {
+        for (int i = 0; i < userCount; i++) {
+            if (users[i].name.equals(name)) {
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    public void listUsers() {
+        if (userCount == 0) {
+            System.out.println("No users found.");
+            return;
+        }
+        System.out.println("\nUsers:");
+        for (int i = 0; i < userCount; i++) {
+            System.out.println("- " + users[i].name);
+        }
+    }
 }
